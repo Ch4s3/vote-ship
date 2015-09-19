fbc1457aa8c828e9f6a45ad0be81008a8ae79e98
Chase <chase.gilliam@gmail.com>
2015-09-19 12:12:59 -0400

    minimal voting

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,31 @@
 class Person < ActiveRecord::Base
   belongs_to :project
+  has_one :vote
+  before_create :generate_url_safe_uuid
+
+  def vote_for(project)
+    return :has_voted if has_voted? == true
+    if project == self.project
+      return :can_not_vote_for_own_project
+    else
+      vote = Vote.new(person: self, project: project)
+      if vote.save
+        self.voted = true
+        vote
+      else
+        :fail
+      end
+    end
+  end
 
   def has_voted?
-    voted == true
+    voted || false
+  end
+
+  def generate_url_safe_uuid
+    return nil if uuid.present?
+    unless uuid.present?
+      self.uuid = SecureRandom.urlsafe_base64(32)
+    end
   end
 end
