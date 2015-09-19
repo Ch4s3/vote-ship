3038805f88f332d1eaf5859a61b148a32ef7b59b
Chase <chase.gilliam@gmail.com>
2015-09-19 15:22:14 -0400

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :contest
   resources :projects
 
-  get "/voting/:uuid", to: "projects#all"
+  get "/voting/:uuid", to: "projects#voting"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
