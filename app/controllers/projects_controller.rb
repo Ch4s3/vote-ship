77fbaa0036ea09d5ba8212c96be2b85209dd168b
Chase <chase.gilliam@gmail.com>
2015-09-19 20:33:27 -0400

    various fixes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,22 +12,26 @@ class ProjectsController < ApplicationController
     @projects = Project.all
     uuid = params[:uuid]
     @person = Person.where(uuid: uuid).first
-  end
-
-  def vote
-    person = Person.find(projects_params[:person_id])
-    if person.has_voted?
-      flash[:note] = "You have already voted"
-      redirct_to :root
-    else
-      project = project.find(projects_params(:project_id))
-      person.vote_for
+    if @person.nil?
+      flash[:danger] = "This token is invalid"
+      redirect_to :root
+    elsif @person.has_voted?
+      flash[:danger] = "You have already voted"
+      redirect_to :root
     end
   end
 
-  private
-
-  def projects_params
-    params.require(:project).permit(:project_id, :person_id, :uuid)
+  def vote
+    uuid = params["uuid"]
+    person = Person.where(uuid: uuid).first
+    if person.has_voted?
+      flash[:info] = "You have already voted"
+      redirect_to :root
+    else
+      project = Project.find(params["project_id"])
+      person.vote_for project
+      flash[:success] = "Thanks for voting!"
+      redirect_to :root
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :projects
 
   get "/voting/:uuid", to: "projects#voting"
+  post "/vote", to: "projects#vote"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
